2eeab65d61e170151b5bb39ec7245a0690dda33c
JuanImmanuelTinambunan <hikariimmanuel10@gmail.com>
2024-11-30 08:38:47 +0700

    Penambahan Halaman Pengajuan Mahasiswa dan perubahan keseluruhan folder
    Jadi, pengajuan mahasiswa sudah siap, baik itu css dan js nya.
    
    Oh ya, aku merapikan lagi keseluruhan foldernya dn penempatannya. Maaf ya

---
+++ Frontend/dashboard/asset/javascript/Pengajuan-mahasiswa.js
@@ -0,0 +1,43 @@
+// ini untuk sidebar
+function toggleSidebar() {
+  const sidebar = document.getElementById("sidebar");
+  const navbar = document.querySelector(".navbar");
+  const mainContent = document.querySelector(".main-content");
+
+  sidebar.classList.toggle("expanded");
+  navbar.classList.toggle("expanded");
+  mainContent.classList.toggle("expanded");
+}
+
+
+//untuk Tombol edit atau banding
+document.getElementById("openFormedit").addEventListener("click", function(){
+  document.querySelector("#formModaledit").style.display = "flex";
+})
+
+document.querySelector("#closeFormedit").addEventListener("click", function(){
+  document.querySelector("#formModaledit").style.display = "none";
+})
+
+//untuk tombol buat pengajuan baru
+document.getElementById("openForm").addEventListener("click", function(){
+  document.querySelector("#formModal").style.display = "flex";
+})
+
+document.querySelector("#closeForm").addEventListener("click", function(){
+  document.querySelector("#formModal").style.display = "none";
+})
+
+//untuk tombol hapus pengajuan
+document.getElementById("openFormdelete").addEventListener("click", function(){
+  document.querySelector("#formModaldelete").style.display = "flex";
+})
+
+document.querySelector("#closeFormdelete").addEventListener("click", function(){
+  document.querySelector("#formModaldelete").style.display = "none";
+})
+
+document.getElementById('showButton').addEventListener('click', function () {
+  document.getElementById('content').style.display = 'block';
+   // Sembunyikan tombol setelah ditekan
+});
