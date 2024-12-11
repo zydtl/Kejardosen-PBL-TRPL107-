3a2997c851e705590f46fdb8f742cf971810a209
yusfppp <computerysf6@gmail.com>
2024-12-11 16:59:06 +0700

    typo hehe

--- Backend/aplikasi-kejardosen/resources/views/dashboard/mahasiswa/jadwal-bimbingan.blade.php
+++ Backend/aplikasi-kejardosen/resources/views/dashboard/mahasiswa/jadwal-bimbingan.blade.php
@@ -23,7 +23,7 @@
         <div class="navbar-right">
             <a href="/"><i class="fi fi-br-bell"></i></a>
             <a href="/"><i class="fi fi-br-power"></i></a>
-            <a href="/"><img src=".{{asset('assets/dashboard/asset/img/avatar.png')}}" alt="User Profile" class="profile-icon"></a>
+            <a href="/"><img src="{{asset('assets/dashboard/asset/img/avatar.png')}}" alt="User Profile" class="profile-icon"></a>
         </div>
     </header>
 
