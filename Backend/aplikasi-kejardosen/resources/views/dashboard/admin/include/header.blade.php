d279010b20624f14cfe7a98d655b7521fb7e6e3e
yusfppp <computerysf6@gmail.com>
2024-12-15 13:01:06 +0700

    Minor fix sidebar-navbar admin blade

--- Backend/aplikasi-kejardosen/resources/views/dashboard/admin/include/header.blade.php
+++ Backend/aplikasi-kejardosen/resources/views/dashboard/admin/include/header.blade.php
@@ -1,7 +1,8 @@
 <header class="navbar">
-    <button onclick="toggleSidebar()" class="toggle-button">☰</button>
+    <!-- HAMBURGER BUTTON -->
+    <button onclick="toggleSidebar()" class="toggle-button">&#9776;</button>
     <div class="navbar-right">
-        <i class="fi fi-br-power"></i>
-        <img src="{{asset('assets/dashboard/asset/img/avatar-admin.png')}}" alt="User Profile" class="profile-icon">
+        <a href="/"><i class="fi fi-br-power"></i></a>
+        <a href="/"><img src="{{asset('assets/dashboard/asset/img/avatar-admin.jpg')}}" alt="User Profile" class="profile-icon" /></a>
     </div>
 </header>
--- Backend/aplikasi-kejardosen/resources/views/dashboard/admin/include/sidebar.blade.php
+++ Backend/aplikasi-kejardosen/resources/views/dashboard/admin/include/sidebar.blade.php
@@ -1,16 +1,27 @@
-<aside id="sidebar" class="sidebar bar">
-    <!-- logo -->
+<aside id="sidebar" class="sidebar">
     <div class="logo-section">
-        <img src="{{ asset('assets/dashboard/asset/img/logokjrdns.png') }}" alt="Kejardosen Logo"
-            class="logokjr" />
-
+        <a href="/"><img src="{{asset('assets/dashboard/asset/img/logokjrdns.png')}}" alt="Kejardosen Logo" class="logokjr" /></a>
     </div>
     <ul class="menu">
-        <li><i class="fi fi-br-home"></i> <span class="menu-text">Beranda</span></li>
-        <li><i class="fi fi-br-graduation-cap"></i> <span class="menu-text">Daftar Mahasiswa</span></li>
-        <li><i class="fi fi-ss-lesson-class"></i> <span class="menu-text">Daftar Dosen</span></li>
-        <li><i class="fi fi-rr-people-network-partner"></i> <span class="menu-text">Hubungkan Mahasiswa</span>
+        <li>
+            <a href="/"><i class="fi fi-br-home"></i>
+                <span class="menu-text">Beranda</span></a>
         </li>
-        <li><i class="fi fi-ss-workshop"></i> <span class="menu-text">Lihat Bimbingan</span></li>
+        <li>
+            <a href="/"><i class="fi fi-br-graduation-cap"></i>
+                <span class="menu-text">Daftar Mahasiswa</span></a>
+        </li>
+        <li>
+            <a href="/"><i class="fi fi-br-lesson-class"></i>
+                <span class="menu-text">Daftar Dosen</span></a>
+        </li>
+        <li>
+            <a href="/"><i class="fi fi-br-people-network-partner"></i>
+                <span class="menu-text">Hubungkan Mahasiswa</span></a>
+        </li>
+        <li>
+            <a href="/"><i class="fi fi-br-workshop"></i>
+                <span class="menu-text">Lihat Bimbingan</span></a>
+        </li>
     </ul>
 </aside>
