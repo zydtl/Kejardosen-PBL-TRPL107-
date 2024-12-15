56497d24f48790f8f63bec73a851e37a459ccb21
yusfppp <computerysf6@gmail.com>
2024-12-15 17:00:06 +0700

    Update sidebar.blade.php
    route siebar dosen done ✅✅

--- Backend/aplikasi-kejardosen/resources/views/dashboard/dosen/include/sidebar.blade.php
+++ Backend/aplikasi-kejardosen/resources/views/dashboard/dosen/include/sidebar.blade.php
@@ -1,12 +1,12 @@
 <aside id="sidebar" class="sidebar">
     <!-- logo -->
     <div class="logo-section">
-        <a href="/"><img src="{{ asset('assets/dashboard/asset/img/logokjrdns.png') }}"
+        <a href="{{ route('dosen.dashboard') }}"><img src="{{ asset('assets/dashboard/asset/img/logokjrdns.png') }}"
                 alt="Kejardosen Logo" class="logokjr" /></a>
     </div>
     <ul class="menu">
         <li>
-            <a href="/"><i class="fi fi-br-home"></i>
+            <a href="{{ route('dosen.dashboard') }}"><i class="fi fi-br-home"></i>
                 <span class="menu-text">Beranda</span></a>
         </li>
         <li>
@@ -14,15 +14,15 @@
                 <span class="menu-text">Pengajuan</span></a>
         </li>
         <li>
-            <a href="/"><i class="fi fi-br-list-check"></i>
+            <a href="{{ route('dosen.jadwal-bimbingan') }}"><i class="fi fi-br-list-check"></i>
                 <span class="menu-text">Jadwal Bimbingan</span></a>
         </li>
         <li>
-            <a href="/"><i class="fi fi-br-calendar"></i>
+            <a href="{{ route('dosen.kalender') }}"><i class="fi fi-br-calendar"></i>
                 <span class="menu-text">Kalender</span></a>
         </li>
         <li>
-            <a href="/"><i class="fi fi-br-memo"></i>
+            <a href="{{ route('dosen.logbook') }}"><i class="fi fi-br-memo"></i>
                 <span class="menu-text">Logbook</span></a>
         </li>
         <li class="menu-item has-submenu">
@@ -32,17 +32,17 @@
             </div>
             <ul class="submenu">
                 <li>
-                    <a href="/"><i class="fi fi-br-time-past"></i>
+                    <a href="{{ route('dosen.riwayat-pengajuan') }}"><i class="fi fi-br-time-past"></i>
                         <span class="menu-text">Riwayat Pengajuan</span></a>
                 </li>
                 <li>
-                    <a href="/"><i class="fi fi-br-time-past"></i>
+                    <a href="{{ route('dosen.riwayat-jadwal-bimbingan') }}"><i class="fi fi-br-time-past"></i>
                         <span class="menu-text">Riwayat Jadwal Bimbingan</span></a>
                 </li>
             </ul>
         </li>
         <li>
-            <a href="/"><i class="fi fi-br-user"></i>
+            <a href="{{ route('dosen.profil') }}"><i class="fi fi-br-user"></i>
                 <span class="menu-text">Profil</span></a>
         </li>
     </ul>
